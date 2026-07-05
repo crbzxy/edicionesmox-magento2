fbdf8e325674fbd382075d77093b6f5a63b5a679
carlos <carlos.boor@gmail.com>
2026-07-05 01:02:17 -0600

    update: mejora de estilos

---
+++ src/app/code/Truper/LandingPage/Controller/Page/View.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Truper\LandingPage\Controller\Page;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class View implements HttpGetActionInterface
+{
+    public function __construct(
+        private readonly PageFactory $pageFactory
+    ) {
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->getConfig()->getTitle()->set(__('Mi nueva página'));
+
+        return $resultPage;
+    }
+}
